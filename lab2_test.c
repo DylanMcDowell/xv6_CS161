0e0d5d990270ded54c3f645fe6bad9772fdd5a19
Dylan McDowell <dmcdo006@sledge.cs.ucr.edu>
2020-11-19 14:10:29 -0800

    Implemented Starvation

--- lab2_test.c
+++ lab2_test.c
@@ -10,9 +10,9 @@ int main(int argc, char *argv[])
       PScheduler();
     else if (atoi(argv[1]) == 2)
       test_donation();
-    else if (atoi(argv[1]) == 3)
-      test_ticktock();
-    exit();
+    //else if (atoi(argv[1]) == 3)
+      //test_ticktock();
+    exit(0);
 }
 /*
 int test_ticktock(void){
@@ -31,10 +31,10 @@ int test_ticktock(void){
             }
             id = gettiktok(&tik, &tok);
             printf(1, " - proc #%d ticks = %d, tocks = %d\n", id, tik, tok);
-            exit();
+            exit(0);
         } else {
             printf(2," \n Error fork() \n");
-            exit();
+            exit(-1);
         }
     }
 
@@ -62,7 +62,7 @@ int test_donation(void){
         }
       }*/
       printf(1, " - child #%d exit with priority %d\n", getpid(), getpriority(getpid()));
-      exit();
+      exit(0);
     }
     if(pid > 0){
       setpriority(10);
@@ -82,8 +82,8 @@ int PScheduler(void){
     int i,j,k;
   
     printf(1, "Testing the priority scheduler and setpriority system call:\n");
-    printf(1, "Assuming that the priorities range between 0 to 255\n");
-    printf(1, "0 is the highest priority. All processes have a default priority of 128\n");
+    printf(1, "Assuming that the priorities range between 0 to 31\n");
+    printf(1, "0 is the highest priority. All processes have a default priority of 10\n");
     printf(1, " - The parent processes will switch to priority 0\n");
     setpriority(0); // Use your own setpriority interface
     for (i = 0; i < 3; i++) {
@@ -91,19 +91,19 @@ int PScheduler(void){
         if (pid > 0) {
             continue;
         } else if ( pid == 0) {
-            printf(1, " - Hello! this is child# %d and I will change my priority to %d \n", getpid(), 240-60*i);
-            setpriority(240-60*i); // Use your own setpriority interface
+            printf(1, " - Hello! this is child# %d and I will change my priority to %d \n", getpid(), 31-5*i);
+            setpriority(31-5*i); // Use your own setpriority interface
             for (j = 0; j < 50000; j++) {
                 for(k = 0; k < 10000; k++) {
                     asm("nop"); 
                 }
             }
-            printf(1, " - Child #%d with priority %d has finished! \n", getpid(), 240-60*i);	
+            printf(1, " - Child #%d with priority %d has finished! \n", getpid(), 31-5*i);	
             printf(1, " - The process with pid %d has a priority of %d after aging.\n", getpid(), getpriority(getpid()));
-            exit();
+            exit(0);
         } else {
             printf(2," \n Error fork() \n");
-            exit();
+            exit(-1);
         }
     }
 
